90d81a509276eeab2dff927f5d62504be48c6b3d
alexdu828 <alexdu828@163.com>
2023-03-17 22:55:36 +0800

    0.9

--- SystemDeploy.sh
+++ SystemDeploy.sh
@@ -8,7 +8,8 @@
 ##----------- "sudo /bin/bash <ScriptName>" -------------------------------------------------##
 
 #variables
-
+zbx_db01=10.10.60.112
+zbx_db02=10.10.60.113
 #begin
 
 
@@ -29,17 +30,6 @@ sudo sh -c 'echo "deb https://mirrors.ustc.edu.cn/postgresql/repos/apt/ $(lsb_re
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
 echo "deb https://packagecloud.io/timescale/timescaledb/ubuntu/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
 
-## 配置Patroni
-## Patroni是一个管理Postgres配置的开源Python软件包. 它可以配置为处理复制,备份和恢复等任务.
-sudo apt install python python3-pip
-mkdir ~/.pip
-cat << EOF > ~/.pip/pip.conf
-[global] 
-index-url = https://pypi.tuna.tsinghua.edu.cn/simple
-[install]
-trusted-host = https://pypi.tuna.tsinghua.edu.cn  # trusted-host 此参数是为了避免麻烦,否则使用的时候可能会提示不受信任
-EOF
-sudo pip install --upgrade setuptools
 
 ## 配置Zabbix APT源 ,建议版本为6.3,
 ## 不采用最新版本的原因是Zabbix与Grafana 9.3插件的兼容性,主要是出现Invalid params. Invalid parameter "/": unexpected parameter "user".错误,https://github.com/alexanderzobnin/grafana-zabbix ISSUS:#1583
@@ -59,31 +49,75 @@ sudo locale-gen
 sudo sed -i 's/en_US/zh_CN/g' /etc/default/locale 
 ###############################################################################################
 
+# 配置时间同步,根据实际情况修改
+sudo echo "NTP=10.10.20.103" >>/etc/systemd/timesyncd.conf
+systemctl restart systemd-timesyncd
 
 
 ###########################################Memo################################################
 ## 数据库群集与负载均衡环境配置,需要联网
-## 3台PGSQLDB节点,采用ClusterControl(Repmgr+实现高可用,2台HAProxy+keepalive节点
+## PGSQLDB+TIMEScaleDB节点,采用Streaming Replication+PgBouncer轻量级中间件实现高可用,2台HAProxy+keepalive节点
+## 关于PgBouncer与Pgpool-II的对比，参考：https://dzone.com/articles/postgresql-connection-pooling-part-4-pgbouncer-vs#:~:text=The%20bottom%20line%20-%20Pgpool-II%20is%20a%20great,and%20reduce%20resource%20consumption%2C%20PgBouncer%20wins%20hands%20down.
 ###############################################################################################
 
-echo -e " \033[032m 
-###############################################################################################
+# 所有PGSQLDB节点安装
+sudo apt -y install postgresql-14
+## 修改PGSQLDB数据存储目录,目录为/pgdata/
+chown -R postgres:postgres /pgdata
+chmod 750 -R /pgdata
+sudo sed -i 's/var\/lib\/postgresql\/14\/main/pgdata\/data/g'  /etc/postgresql/14/main/postgresql.conf
+sudo -u postgres /usr/lib/postgresql/14/bin/initdb -D /pgdata/data   
+sudo systemctl restart postgresql
+sudo -u postgres psql 
+show data_directory;
+# postgres=# show data_directory;
+#  data_directory
+# ----------------
+#  /pgdata/data
+# (1 行记录)
 
-|  __ \|  ____|  __ \| |    / __ \ \   / / |  __ \|  _ \|  ____| \ | \ \    / /
-| |  | | |__  | |__) | |   | |  | \ \_/ /  | |  | | |_) | |__  |  \| |\ \  / / 
-| |  | |  __| |  ___/| |   | |  | |\   /   | |  | |  _ <|  __| | .   | \ \/ /  
-| |__| | |____| |    | |___| |__| | | |    | |__| | |_) | |____| |\  |  \  /   
-|_____/|______|_|    |______\____/  |_|    |_____/|____/|______|_| \_|   \/                                          
-    
-###############################################################################################\033[00m "
-echo -e "\033[031m____________________________________________ \033[00m"							     
-echo -e "\033[031m|##########################################| \033[00m"							     
-echo -e "\033[031m|##as a sudo user who got root privileges##| \033[00m"							     
-echo -e "\033[031m|##########################################| \033[00m"							     
-echo -e "\033[031m____________________________________________ \033[00m"
+# 配置TIMEScaleDB,参考：https://docs.timescale.com/install/latest/self-hosted/installation-linux/
+sudo apt install -y timescaledb-2-postgresql-14
+## 自动调整postgresql配置参数 
+sudo timescaledb-tune --quiet
+systemctl restart postgresql
+
+# 配置Streaming Replication
+
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '$zbx_db01'/g" /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/#wal_level/wal_level/g' /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/#max_wal_senders/max_wal_senders/g' /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/#wal_keep_size = 0/wal_keep_size = 1GB/g' /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/#wal_compression = off/wal_compression = on/g' /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/#synchronous_commit = on/synchronous_commit = on/g' /etc/postgresql/14/main/postgresql.conf
 
 
-## 3台PGSQLDB节点安装
+# host replication [replication user] [allowed network] [authentication method]
+# echo "host    all             all             0.0.0.0/0             md5">>/etc/postgresql/14/main/pg_hba.conf                 #允许所有网络访问
+echo "host    replication     rep_user        10.10.60.112/32            md5" >>/etc/postgresql/14/main/pg_hba.conf
+echo "host    replication     rep_user        10.10.60.113/32            md5" >>/etc/postgresql/14/main/pg_hba.conf
+
+# 创建rep_user用户
+su postgres 
+createuser --replication -P rep_user
+systemctl restart postgresql
+
+# 备节点操作
+#variables
+zbx_db01=10.10.60.112
+zbx_db02=10.10.60.113
+#begin
+systemctl stop postgresql
+rm -rf /pgdata/data/*
+sudo -u postgres pg_basebackup -R  -h $zbx_db01 -U rep_user -v -D /pgdata/data/ -P 
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '$zbx_db02'/g" /etc/postgresql/14/main/postgresql.conf
+sudo sed -i "s/#hot_standby = on/hot_standby = on/g" /etc/postgresql/14/main/postgresql.conf
+systemctl start postgresql
+# 主节点检查
+
+sudo -U postgres psql 
+select usename, application_name, client_addr, state, sync_priority, sync_state from pg_stat_replication;
+
 
 
 # 数据库权限修复
